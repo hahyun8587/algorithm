fb193712a326cc0b0eeb31c23cdf878597256dbf
백하현 <hahyun8587@gmail.com>
2019-12-13 15:33:51 +0900

    solved

---
+++ DP/BOJ2748.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define MAX_NUM 90
+
+typedef long long ll;
+
+ll fibo(int n);
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    printf("%lld\n", fibo(n));
+
+    return 0;
+}
+
+ll fibo(int n)
+{
+    ll dp[MAX_NUM + 1];
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= MAX_NUM; i++) 
+        dp[i] = dp[i - 1] + dp[i - 2];
+    
+    return dp[n];
+}
