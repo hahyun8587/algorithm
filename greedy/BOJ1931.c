736ba2dcd6b74c4feba1f468e5460dcd424e06b0
Guest <guest-5wqdsx@h11317.sgb.skir.kr>
2021-04-15 23:58:09 +0900

    solving

--- greedy/BOJ1931.c
+++ greedy/BOJ1931.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
+#include <malloc.h>
 
+#define ARR_SIZE 2
 #define MAX_SIZE 100000
+#define STD1 1
+#define STD2 0
 
 typedef struct {
-    int *ha;
+    int **ha;
     int n;
 } heap;
 
-heap* initHeap(int n);
+heap* initHeap(int (*arr)[ARR_SIZE], int n);
+int argMax(int (*arr)[ARR_SIZE], int i, int j);
 void sink(heap *h, int s);
 void maxHeap(heap *h);
 void delete(heap *h);
 void heapSort(heap *h);
-int maxConf(int (*arr)[]);
+int maxConf(int (*arr)[ARR_SIZE]);
+
+int main() {
+
+
+
+    return 0;
+}
+
+heap* initHeap(int (*arr)[ARR_SIZE], int n) {
+    heap *h;
+
+    h = (heap *) malloc(sizeof(heap));        
+    h->ha = arr;
+    h->n = n;
+
+    return h;
+}
+
+int argMax(int (*arr)[ARR_SIZE], int i, int j) {
+    if (arr[i][STD1] > arr[j][STD1])
+        return i;
+    else if (arr[i][STD1] < arr[j][STD1])
+        return j;
+    else
+        return arr[i][STD2] > arr[j][STD2] ? i : j;         
+}
+
+void sink(heap *h, int s) {
+
+
+
+
+}
