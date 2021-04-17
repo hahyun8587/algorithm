c48fd1f61a863454e5529cd6c5c5d1f5b2a245af
Guest <hahyun8587@gmail.com>
2021-04-17 10:02:48 +0900

    solving

--- greedy/BOJ1931.c
+++ greedy/BOJ1931.c
@@ -9,11 +9,12 @@
 typedef struct {
     int **ha;
     int n;
+    int m;
 } heap;
 
 heap* initHeap(int (*arr)[ARR_SIZE], int n);
 int argMax(int (*arr)[ARR_SIZE], int i, int j);
-void swap(int *a, int *b);
+void swap(int **a, int **b);
 void sink(heap *h, int s);
 void maxHeap(heap *h);
 void delete(heap *h);
@@ -33,6 +34,7 @@ heap* initHeap(int (*arr)[ARR_SIZE], int n) {
     h = (heap *) malloc(sizeof(heap));        
     h->ha = arr;
     h->n = n;
+    h->m = ARR_SIZE;
 
     return h;
 }
@@ -46,8 +48,8 @@ int argMax(int (*arr)[ARR_SIZE], int i, int j) {
         return arr[i][STD2] > arr[j][STD2] ? i : j;         
 }
 
-void swap(int *a, int *b) {
-    int temp;
+void swap(int **a, int **b) {
+    int *temp;
 
     temp = *a;
     *a = *b;
@@ -56,11 +58,37 @@ void swap(int *a, int *b) {
 
 void sink(heap *h, int s) {
     int curr = s;
+    int std;
 
     while(2 * curr <= h->n - 1) {
-            
+        if (2 * curr < h->n - 1)
+            std = argMax(h->ha, 2 * curr, 2 * curr + 1);
+        else 
+            std = 2 * curr;
+
+        if (h->ha[curr][STD1] < h->ha[std][STD1])
+            swap(&h->ha[curr], &h->ha[std]);
+        else if (h->ha[curr][STD1] == h->ha[std][STD1]) {
+            if (h->ha[curr][STD2] < h->ha[std][STD2]) 
+                swap(h->ha[curr], h->ha[std]);
+            else 
+                break;
+        }
+        else
+            break;
+
+        curr = std;    
     }
+}
+
+void maxHeap(heap *h) {
+    for(int i = (h->n - 1) / 2; i > 0; i--)
+        sink(h, i);
+}
+
+void delete(heap *h) {
+    
 
 
-
+    
 }
