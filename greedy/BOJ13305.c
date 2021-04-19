b973439be235a2bd6b3f4f243dd946890ebb475b
Guest <hahyun8587@gmail.com>
2021-04-19 19:50:31 +0900

    solving

---
+++ greedy/BOJ13305.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_SIZE 100000
+#define MAX_DIGIT 19
+
+typedef long long ll;
+
+void minCost(int *src1, int n, int *src2, ll *dst);
+
+int main() {
+
+
+
+    return 0;
+}
+
+void minCost(int *src1, int n, int *src2, ll *dst) {
+    ll cost;
+    int divider;
+    ll fwd = 0, bwd = 0;
+
+    divider = (int) pow(10, MAX_DIGIT / 2);
+
+    
+
+
+
+
+
+}
