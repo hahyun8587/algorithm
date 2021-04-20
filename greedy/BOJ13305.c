2a57de35b0b37db3b0c3890beaaf65237367b0b1
Guest <guest-hvjcip@h11317.sgb.skir.kr>
2021-04-20 19:44:20 +0900

    solved

--- greedy/BOJ13305.c
+++ greedy/BOJ13305.c
@@ -6,40 +6,41 @@
 
 typedef long long ll;
 
-void minCost(int *src1, int n, int *src2, ll *dst, int spliter);
+void minCost(ll *src1, int n, ll *src2, ll *dst, int spliter);
 int digit(int a);
-void printL(ll fwd, int bwd);
+void lprintf(ll fwd, int bwd);
 
 int main() {
     int N;
-    int dist[MAX_SIZE - 1];
-    int cost[MAX_SIZE];
+    ll dist[MAX_SIZE - 1];
+    ll cost[MAX_SIZE];
     ll mCost[2] = {0, 0};
     int spliter;
 
     scanf("%d", &N);
 
     for (int i = 0; i <= N - 2; i++)
-        scanf("%d", &dist[i]);
+        scanf("%lld", &dist[i]);
 
     for (int i = 0; i < N; i++) 
-        scanf("%d", &cost[i]);
+        scanf("%lld", &cost[i]);
 
     spliter = (int) pow(10, MAX_DIGIT / 2);    
 
     minCost(dist, N - 1, cost, mCost, spliter);
+    lprintf(mCost[0], mCost[1]);
 
     return 0;
 }
 
-void minCost(int *src1, int n, int *src2, ll *dst, int spliter) {
+void minCost(ll *src1, int n, ll *src2, ll *dst, int spliter) {
     ll cpc;
     int cpl;
 
     cpl = src2[0];
 
     for (int i = 0; i < n; i++) {
-        cpc = (ll) (cpl * src1[i]);
+        cpc = cpl * src1[i];
         dst[0] += cpc / spliter;
         dst[1] += cpc % spliter;
 
@@ -56,16 +57,16 @@ int digit(int a) {
     do {
         a /= 10;
         count++;
-    } while(a);
+    } while (a);
 
     return count;
 }
 
-void printL(ll fwd, int bwd) {
+void lprintf(ll fwd, int bwd) {
     if (fwd) {
-        printf("%d", fwd);
+        printf("%lld", fwd);
 
-        for(int i = 1; i <= MAX_DIGIT / 2 - digit(bwd); i++)
+        for (int i = 1; i <= MAX_DIGIT / 2 - digit(bwd); i++)
             printf("0");
 
         printf("%d", bwd);    
