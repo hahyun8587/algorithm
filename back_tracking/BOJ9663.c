af447c19a223fdb7fd791d7a622facf287ad15ea
Guest <hahyun8587@gmail.com>
2021-04-24 23:52:44 +0900

    solving

--- back_tracking/BOJ9663.c
+++ back_tracking/BOJ9663.c
@@ -2,18 +2,54 @@
 
 #define MAX_SIZE 14
 
-int nQueen(int (*visited)[MAX_SIZE], int n, int row);
+void initArr(int (*arr)[MAX_SIZE], int n);
+void denote(int (*visited)[MAX_SIZE], int n, int x, int y, int val);
+int nQueen(int (*visited)[MAX_SIZE], int n, int x, int y);
 
 int main() {
+    int N;
+    int visited[MAX_SIZE][MAX_SIZE];
+    int count = 0;
 
+    scanf("%d", &N);
+    initArr(visited, N);
 
+    for (int i = 0; i < N; i++) 
+        count += nQueen(visited, N, 0, i);
+
+    printf("%d\n", count);    
 
     return 0;
 }
 
-int nQueen(int (*visited)[MAX_SIZE], int n, int row) {
-    
+void initArr(int (*arr)[MAX_SIZE], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) 
+            arr[i][j] = 0;
+    }
+}
 
+void denote(int (*visited)[MAX_SIZE], int n, int x, int y, int val) {
+    for (int i = x + 1; i < n; i++) {
+        visited[i][-i + x + y] = val;
+        visited[i][y] = val;
+        visited[i][i - x + y] = val;
+    }   
+}
 
+int nQueen(int (*visited)[MAX_SIZE], int n, int x, int y) {
+    if (x == n - 1) 
+        return 1;
 
+    int count = 0;
+
+    denote(visited, n, x, y, 1);
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[x + 1][i])
+            count += nQueen(visited, n, x + 1, i);            
+    }
+    denote(visited, n, x, y, 0);
+
+    return count;
 }
