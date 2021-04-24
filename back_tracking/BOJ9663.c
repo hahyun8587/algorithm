0515d77be56d0059d8d9e5efda8e0312fa88aa9f
Guest <hahyun8587@gmail.com>
2021-04-24 20:22:19 +0900

    solving

---
+++ back_tracking/BOJ9663.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define MAX_SIZE 14
+
+int nQueen(int (*visited)[MAX_SIZE], int n, int row);
+
+int main() {
+
+
+
+    return 0;
+}
+
+int nQueen(int (*visited)[MAX_SIZE], int n, int row) {
+    
+
+
+
+}
