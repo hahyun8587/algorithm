41de590fdfbd724e28af1461ad2c1b2f83930385
Guest <hahyun8587@gmail.com>
2021-04-26 18:46:44 +0900

    solved

--- DP/BOJ2579.c
+++ DP/BOJ2579.c
@@ -25,50 +25,16 @@ int max(int a, int b) {
 }
 
 int maxScore(int *score, int n) {
-    int dp[MAX_SIZE + 1][LIMIT][MAX_SIZE + 1];
-    int ans = 0;
-
-    dp[0][0][0] = 0;
-
-    for (int i = 0; i < LIMIT; i++) {
-        for (int j = -i + 1; j <= n; j++)
-            dp[0][i][j] = -1;
-    }
-
-    for (int i = 0; i < LIMIT; i++) {
-        for (int j = 0; j <= n; j++) 
-            dp[1][i][j] = !i && (j == 1 || j == 2) ? score[j - 1] : -1;
-    }
+    int dp[MAX_SIZE + 1][LIMIT];
+    
+    dp[0][0] = 0;
+    dp[0][1] = -1;
+    dp[1][0] = score[0];
+    dp[1][1] = -1;
 
     for (int i = 2; i <= n; i++) {
-        for (int j = 0; j < LIMIT; j++) {
-            for (int k = 0; k <= -j + 1; k++) 
-                dp[i][j][k] = -1;
-        }
-    }
-
-    for (int i = 2; i <= n; i++) {
-        for (int j = 0; j < LIMIT; j++) {
-            for (int k = -j + 2; k <= n; k++) {
-                if (!j) {
-                    if (dp[i - 1][j][k - 2] != -1 && dp[i - 1][j + 1][k - 2] != -1)
-                        dp[i][j][k] = max(dp[i - 1][j][k - 2], dp[i - 1][j + 1][k - 2]) + score[k - 1];
-                    else if (dp[i - 1][j][k - 2] == -1 && dp[i - 1][j + 1][k - 2] != -1)
-                        dp[i][j][k] = dp[i - 1][j + 1][k - 2] + score[k - 1];
-                    else if (dp[i - 1][j][k - 2] != -1 && dp[i - 1][j + 1][k - 2] == -1)
-                        dp[i][j][k] = dp[i - 1][j][k - 2] + score[k - 1];
-                    else 
-                        dp[i][j][k] = -1;        
-                }
-                else 
-                    dp[i][j][k] = dp[i - 1][j - 1][k - 1] != -1 ? dp[i - 1][j - 1][k - 1] + score[k - 1] : -1;
-            }
-        }
-    }
-
-    for (int i = n / 2; i <= n; i++) {
         for (int j = 0; j < LIMIT; j++) 
-            ans = max(dp[i][j][n], ans);
+            dp[i][j] = !j ? max(dp[i - 2][j], dp[i - 2][j + 1]) + score[i - 1] : dp[i - 1][0] + score[i - 1];
     }
-    return ans;
+    return max(dp[n][0], dp[n][1]);
 }
