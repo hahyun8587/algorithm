124061e99fec48bb122bda4c4064e0981cdce521
Guest <hahyun8587@gmail.com>
2021-04-28 20:27:40 +0900

    solving

---
+++ back_tracking/BOJ14889.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define MAX_SIZE 20
+
+int calc(int *stats, int *teammate, int count);
+void _MDS(int *stats, int *visited, int n, int *teammate, int count, int sum);
+
+int main() {
+
+
+
+    return 0;
+}
