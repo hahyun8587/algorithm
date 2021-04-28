3215721148105beeb5ca9088668da8b2fd42d57a
Guest <hahyun8587@gmail.com>
2021-04-28 20:08:25 +0900

    solved

--- back_tracking/BOJ14888.c
+++ back_tracking/BOJ14888.c
@@ -3,15 +3,31 @@
 #define MAX_SIZE 11
 #define NUM_OP 4
 #define NUM_RESULT 2
+#define INF 1000000000
 
 int calc(int oprnd1, int oprnd2, int oprtr);
 int max(int a, int b);
 int min(int a, int b);
-void embed(int *src, int n, int *numOp, int *dst, int count, int op, int result);
+void _embed(int *src, int n, int *numOp, int *dst, int count, int op, int result);
+void embed(int *src, int n, int *numOp, int *dst);
 
 int main() {
+    int N;
+    int oprnd[MAX_SIZE], numOprtr[NUM_OP];
+    int ext[NUM_RESULT] = {-INF, INF};
 
+    scanf("%d", &N);
 
+    for (int i = 0; i < N; i++)
+        scanf("%d", &oprnd[i]);
+
+    for (int i = 0; i < NUM_OP; i++)
+        scanf("%d", &numOprtr[i]);
+
+    embed(oprnd, N, numOprtr, ext);    
+    
+    for (int i = 0; i < NUM_RESULT; i++)
+        printf("%d\n", ext[i]);
 
     return 0;
 }
@@ -44,7 +60,7 @@ int min(int a, int b) {
     return a < b ? a : b;
 }
 
-void embed(int *src, int n, int *numOp, int *dst, int count, int op, int result) {
+void _embed(int *src, int n, int *numOp, int *dst, int count, int op, int result) {
     result = calc(result, src[count + 1], op);
     numOp[op]--;
     count++;
@@ -52,15 +68,21 @@ void embed(int *src, int n, int *numOp, int *dst, int count, int op, int result)
     if (count == n - 1) {
         dst[0] = max(result, dst[0]);
         dst[1] = min(result, dst[1]);
+        numOp[op]++;
 
         return;
     }
 
     for (int i = 0; i < NUM_OP; i++) {
         if (numOp[i])
-            embed(src, n, numOp, dst, count, i, result);          
+            _embed(src, n, numOp, dst, count, i, result);          
     }
-    
-    
+    numOp[op]++;
 }
 
+void embed(int *src, int n, int *numOp, int *dst) {
+    for (int i = 0; i < NUM_OP; i++) {
+        if (numOp[i]) 
+            _embed(src, n, numOp, dst, 0, i, src[0]);
+    }   
+}
