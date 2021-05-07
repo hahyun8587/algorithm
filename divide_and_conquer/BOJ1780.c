156f79c560ce7d1ef9fe0889094eca242c310a16
Guest <hahyun8587@gmail.com>
2021-05-07 20:14:54 +0900

    solved

--- divide_and_conquer/BOJ1780.c
+++ divide_and_conquer/BOJ1780.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
-#include <stdio.h>
-
-#define MAX_SIZE 128
+#define MAX_SIZE 2187
 #define NUM_TYPE 3
+#define NUM_DIVISION 9
 
 void divide(int (*src)[MAX_SIZE], int *dst, int xs, int xe, int ys, int ye);
 
 int main() {
-  
+    int arr[MAX_SIZE][MAX_SIZE];
+    int num[] = {0, 0, 0};
+    int N;
 
+    scanf("%d", &N);
 
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            scanf("%d", &arr[i][j]);
+    } 
+    divide(arr, num, 0, N - 1, 0, N - 1);
 
+    for (int i = 0; i < NUM_TYPE; i++)
+        printf("%d\n", num[i]);
 
     return 0;
 }
@@ -33,13 +42,13 @@ void divide(int (*src)[MAX_SIZE], int *dst, int xs, int xe, int ys, int ye) {
     }
 
     if (isSolid) {
-        dst[val]++;
+        dst[val + 1]++;
 
         return;
     }
 
     int n = xe - xs + 1;
-    
-    
 
+    for (int i = 0; i < NUM_DIVISION; i++)
+        divide(src, dst, xs + n / 3 * (i / 3), xs + (i / 3 + 1) * n / 3 - 1, ys + n / 3 * (i % 3), ys + (i % 3 + 1) * n / 3 - 1);
 }
