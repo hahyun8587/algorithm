a83d7e8bdf137fc3256087a281e4a14b4c6a667b
Guest <hahyun8587@gmail.com>
2021-05-08 15:56:18 +0900

    solved

--- divide_and_conquer/BOJ1629.c
+++ divide_and_conquer/BOJ1629.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
-int divide(int a, int b, int c);
-int combine(int val, int a, int b, int c);
+typedef long long ll;
+
+ll combine(ll val, int a, int b, int c);
+ll divide(int a, int b, int c);
 
 int main() {
-    
+    int A, B, C;
 
+    scanf("%d %d %d", &A, &B, &C);
+    printf("%lld\n", divide(A, B, C));
 
     return 0;
 }
 
-int combine(int val, int a, int b, int c) {
-    return val * val * (1 + (c - 1) * (b % 2)) % c;
+ll combine(ll val, int a, int b, int c) {
+    return val * val % c * ((1 + (a - 1) * (b % 2)) % c) % c;
 }
 
-int divide(int a, int b, int c) {
+ll divide(int a, int b, int c) {
     if (b == 1)
         return a % c;
 
-    int val;
+    ll val;
     
     val = !(b % 2) ? divide(a, b / 2, c) : divide(a, (b - 1) / 2, c);
 
