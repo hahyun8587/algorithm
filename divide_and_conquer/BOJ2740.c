ebc6eeb28def450cc56cece09db07fc8d6381d6c
Guest <hahyun8587@gmail.com>
2021-05-09 20:28:32 +0900

    solving

--- divide_and_conquer/BOJ2740.c
+++ divide_and_conquer/BOJ2740.c
@@ -2,23 +2,30 @@
 
 #define MAX_SIZE 100
 
-void getArr2D(int (*arr)[MAX_SIZE], int n, int m);
+void getArr2D(int (*arr)[MAX_SIZE], int *n, int *m);
 void dot(int (*src1)[MAX_SIZE], int n, int m, int (*src2)[MAX_SIZE], int p, int o, int (*dst)[MAX_SIZE]);
+void aprintf(int (*arr)[MAX_SIZE], int n, int m);
 
 int main() {
     int arr1[MAX_SIZE][MAX_SIZE], arr2[MAX_SIZE][MAX_SIZE];
+    int product[MAX_SIZE][MAX_SIZE];
     int N, M, K;
 
-    
+    getArr2D(arr1, &N, &M);
+    getArr2D(arr2, &M, &K);
+    dot(arr1, N, M, arr2, M, K, product);
+    aprintf(product, N, K);
 
     return 0;
 }
 
-void getArr2D(int (*arr)[MAX_SIZE], int n, int m) {
-    
+void getArr2D(int (*arr)[MAX_SIZE], int *n, int *m) {
+    scanf("%d %d", n, m);
 
-
-
+    for (int i = 0; i < *n; i++) {
+        for (int j = 0; j < *m; j++) 
+            scanf("%d", &arr[i][j]);
+    }   
 }
 
 void dot(int (*src1)[MAX_SIZE], int n, int m, int (*src2)[MAX_SIZE], int p, int o, int (*dst)[MAX_SIZE]) {
@@ -31,7 +38,16 @@ void dot(int (*src1)[MAX_SIZE], int n, int m, int (*src2)[MAX_SIZE], int p, int 
             for (int k = 0; k < p; k++) 
                sum += src1[i - 1][k] * src2[k][j]; 
 
-            dst[(i - 1) / o][(i - 1) % o] = sum;   
+            dst[(i - 1) / o][(i - 1) % o] = sum;  
         }
+    }
+}
+
+void aprintf(int (*arr)[MAX_SIZE], int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) 
+            printf("%d ", arr[i][j]);
+
+        putchar('\n');    
     }
 }
