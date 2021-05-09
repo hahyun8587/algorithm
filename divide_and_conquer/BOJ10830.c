c3248023ad1a92ad3ea87cdeabc66e0a76123cef
Guest <hahyun8587@gmail.com>
2021-05-09 23:34:50 +0900

    solving

---
+++ divide_and_conquer/BOJ10830.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MAX_SIZE 5
+#define MODULOR 1000
+
+void dot(int (*src1)[MAX_SIZE], int n, int m, int (*src2)[MAX_SIZE], int p, int o, int (*dst)[MAX_SIZE]);
+void clone(int (*src)[MAX_SIZE], int n, int (*dst)[MAX_SIZE]);
+void calc(int (*src1)[MAX_SIZE], int n, int (*src2)[MAX_SIZE], int (*dst)[MAX_SIZE]);
+void mpow(int (*src)[MAX_SIZE], int n, int (*dst)[MAX_SIZE], int b);
+void aprintf(int (*arr)[MAX_SIZE], int n);
+
+int main() {
+
+
+
+    return 0;
+}
+
+void clone(int (*src)[MAX_SIZE], int n, int (*dst)[MAX_SIZE]) {
+        
+
+
+
+}
+
