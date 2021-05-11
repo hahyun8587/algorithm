62a19106cf1387351558f3484ae16c7cabc30b9c
Guest <hahyun8587@gmail.com>
2021-05-11 20:28:46 +0900

    solving

--- divide_and_conquer/BOJ10830.c
+++ divide_and_conquer/BOJ10830.c
@@ -5,10 +5,7 @@
 
 typedef long long ll;
 
-int product[MAX_SIZE][MAX_SIZE];
-
 int (*dot(int (*arr1)[MAX_SIZE], int n, int m, int (*arr2)[MAX_SIZE], int p, int o))[MAX_SIZE];
-int (*mmodulo(int (*arr)[MAX_SIZE], int n, int m, int c))[MAX_SIZE];
 int (*mpow(int (*arr)[MAX_SIZE], int n, ll b))[MAX_SIZE];
 void mprintf(int (*arr)[MAX_SIZE], int n, int m);
 
@@ -30,27 +27,21 @@ int main()
 }
 
 int (*dot(int (*src1)[MAX_SIZE], int n, int m, int (*src2)[MAX_SIZE], int p, int o))[MAX_SIZE] {
+    static int product[MAX_SIZE][MAX_SIZE];
     int sum;
 
     for (int i = 1; i <= n; i++) {
         for (int j = 0; j < o; j++) {
             sum = 0;
 
-            for (int k = 0; k < p; k++) 
-               sum += src1[i - 1][k] * src2[k][j]; 
-
-            product[i - 1][j] = sum;  
+            for (int k = 0; k < p; k++) {
+                sum += src1[i - 1][k] * src2[k][j]; 
+                printf("%d\n", sum); 
+            }
+            product[i - 1][j] = sum; 
         }
     }
     return product;
-}
-
-int (*mmodulo(int (*arr)[MAX_SIZE], int n, int m, int c))[MAX_SIZE] {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) 
-            arr[i][j] %= c;
-    }
-    return arr;
 }
 
 int (*mpow(int (*arr)[MAX_SIZE], int n, ll b))[MAX_SIZE] {
@@ -60,8 +51,10 @@ int (*mpow(int (*arr)[MAX_SIZE], int n, ll b))[MAX_SIZE] {
     int (*val)[MAX_SIZE];
 
     val = !(b % 2) ? mpow(arr, n, b / 2) : mpow(arr, n, (b - 1) / 2);     
-   
-    return !(b % 2) ? mmodulo(dot(val, n, n, val, n, n), n, n, MODULOR) : mmodulo(dot(mmodulo(dot(val, n, n, val, n, n), n, n, MODULOR), n, n, arr, n, n), n, n, MODULOR);
+
+    //mprintf(val, n, n);
+
+    return !(b % 2) ? dot(val, n, n, val, n, n) : dot(dot(val, n, n, val, n, n), n, n, arr, n, n);
 }
 
 void mprintf(int (*arr)[MAX_SIZE], int n, int m) {
