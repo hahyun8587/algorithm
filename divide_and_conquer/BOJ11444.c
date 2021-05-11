92c3a753e5c2b4c03aae94e8c648fb6877019c26
Guest <hahyun8587@gmail.com>
2021-05-11 23:55:31 +0900

    solving

---
+++ divide_and_conquer/BOJ11444.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MODULOR 1000000007
+#define ARR_SIZE 2
+
+typedef long long ll;
+
+
+int (*dot(int (*src1)[ARR_SIZE], int n, int m, int (*src2)[ARR_SIZE], int p, int o))[ARR_SIZE];
+int (*mpow(int (*arr)[ARR_SIZE], int n, ll b))[ARR_SIZE];
+int fibo(ll n);
+
+int main() {
+
+
+
+    return 0;
+}
+
+int fibo(ll n) {
+
+
+
+
+}
