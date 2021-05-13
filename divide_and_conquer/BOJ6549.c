e457ce9b943a1951b1ae5d4fa995c08b878cebd9
Guest <hahyun8587@gmail.com>
2021-05-13 20:26:54 +0900

    solving

---
+++ divide_and_conquer/BOJ6549.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100000
+
+typedef long long ll;
+
+ll max(ll a, ll b);
+ll maxBound(int *arr, int s, int mid, int e);
+ll _maxArea(int *arr, int n, int s, int e);
+ll maxArea(int *arr, int n);
+
+int main() {
+
+
+
+    return 0;
+}
+
+ll max(ll a, ll b) {
+    return a > b ? a : b;
+}
+
+ll maxBound(int *arr, int s, int mid, int e) {
+    
+
+
+
+}
+
+
+ll _maxArea(int *arr, int n, int s, int e) {
+
+
+
+
+}
