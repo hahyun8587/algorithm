f10cb5567995bd8c8e0c7caee90b738a3c143214
Guest <hahyun8587@gmail.com>
2021-05-15 11:38:26 +0900

    solving

--- divide_and_conquer/BOJ6549.c
+++ divide_and_conquer/BOJ6549.c
@@ -4,6 +4,7 @@
 
 typedef long long ll;
 
+int min(int a, int b);
 ll max(ll a, ll b);
 ll maxBound(int *arr, int s, int mid, int e);
 ll _maxArea(int *arr, int n, int s, int e);
@@ -16,12 +17,25 @@ int main() {
     return 0;
 }
 
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
 ll max(ll a, ll b) {
     return a > b ? a : b;
 }
 
 ll maxBound(int *arr, int s, int mid, int e) {
-    
+    int minHei;
+    int left = mid + 1;
+    int right = mid + 2;
+    ll maxArea;
+
+    minHei = min(arr[mid], arr[mid + 1]);
+    maxArea = (ll) (right - (left + 1)) * minHei; 
+
+    while ()
+
 
 
 
