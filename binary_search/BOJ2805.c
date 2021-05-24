74900fa41c514567de35c5114916a39e65f715ac
Guest <hahyun8587@gmail.com>
2021-05-24 20:23:40 +0900

    solving

---
+++ binary_search/BOJ2805.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define MAX_SIZE 1000000
+
+int max(int a, int b);
+int check(int *arr, int n, int mid, int t);
+int _maxHei(int *arr, int n, int s, int e, int t);
+int maxHei(int *arr, int n);
+
+int main() {
+
+
+
+    return 0;
+}
+
