3bc7544d918d7de5faf0fa4740d78786ab07c344
Guest <hahyun8587@gmail.com>
2021-05-28 19:53:17 +0900

    solved

--- binary_search/BOJ2110.c
+++ binary_search/BOJ2110.c
@@ -12,7 +12,7 @@ typedef struct {
 int ascn(int *arr, int i, int j);
 heap* initHeap(int *arr, int n);
 void swap(int *a, int *b);
-void sink(heap *h);
+void sink(heap *h, int s);
 void heapify(heap *h);
 void delete(heap *h);
 void heapSort(heap *h);
@@ -35,22 +35,91 @@ int main() {
 
     heapSort(h);
     free(h);
-    printf("%d\n", maxDist(arr, N + 1, C));
+    printf("%d\n", maxDist(&arr[1], N, C));
 
     return 0;
 }
 
+int ascn(int *arr, int i, int j) {
+    return arr[i] > arr[j] ? 0 : 1;
+}
+
+heap* initHeap(int *arr, int n) {
+    heap *h;
+
+    h = (heap *) malloc(sizeof(heap));
+    h->ha = arr;
+    h->n = n;
+    h->type = ascn;
+
+    return h;
+}
+
+void swap(int *a, int *b) {
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sink(heap *h, int s) {
+    int curr;
+    int cmp;
+
+    curr = s;
+
+    while (curr * 2 <= h->n - 1) {
+        if (curr * 2 == h->n - 1)
+            cmp = curr * 2;
+        else
+            cmp = !h->type(h->ha, curr * 2, curr * 2 + 1) ? curr * 2 : curr * 2 + 1;
+        
+        if (h->type(h->ha, curr, cmp)) {
+            swap(&h->ha[curr], &h->ha[cmp]);
+
+            curr = cmp;    
+        }
+        else
+            break;
+    }
+}
+
+void heapify(heap *h) {
+    for (int i = (h->n - 1) / 2; i >= 1; i--)
+        sink(h, i);
+}
+
+void delete(heap *h) {
+    swap(&h->ha[1], &h->ha[h->n-- - 1]);
+    sink(h, 1);
+}
+
+void heapSort(heap *h) {
+    int n = h->n;
+
+    heapify(h);
+
+    for (int i = 1; i < n; i++) 
+        delete(h);  
+
+    h->n = n; 
+}
+
 int check(int *arr, int n, int d, int c) {
-    int count = 1;    
     int ds = 0;
+    int count = 1;    
 
     for (int i = 1; i < n; i++) {
         if (arr[i] - arr[ds] >= d) {
             ds = i;
             count++;
         }
+
+        if (count == c)
+            return 1;
     }   
-    return count >= c ? 1 : 0;
+    return 0;
 }
 
 int max(int a, int b) {
