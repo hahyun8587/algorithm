2d92f3b5b227f08475ea9127bd8c5f57c0512ae9
Guest <hahyun8587@gmail.com>
2021-05-29 20:18:18 +0900

    solving

--- binary_search/BOJ1300.c
+++ binary_search/BOJ1300.c
@@ -12,8 +12,34 @@ int main() {
 }
 
 int check(int n, int x, int k) {
-            
+    int ns = 0, nx = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int q = x / i;
+        int r = x % i;
+
+        if (i < q || i == q && r) 
+            ns += 2 * i - 1;
+        else if (i == q && !r) {
+            ns += 2 * (i - 1);
+            nx++;
+        }
+        else if (i > q && !r) {
+            ns += 2 * (q - 1);
+            nx += 2;
+        }    
+        else 
+            ns += 2 * q;
+
+        if (ns >= k)
+            return 0;    
+    }          
+    return ns + nx < k ? 1 : 2;
+}
+
+int _access(int n, int s, int e, int k) {
 
 
 
+    
 }
