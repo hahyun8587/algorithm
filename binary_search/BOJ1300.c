b5a78fa827a9d66afc2a1886c4c85eab8f3aedcb
Guest <hahyun8587@gmail.com>
2021-05-29 13:46:36 +0900

    solving

---
+++ binary_search/BOJ1300.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int check(int n, int x, int k);
+int _access(int n, int s, int e, int k);
+int access(int n, int k);
+
+int main() {
+
+
+
+    return 0;
+}
+
+int check(int n, int x, int k) {
+            
+
+
+
+}
