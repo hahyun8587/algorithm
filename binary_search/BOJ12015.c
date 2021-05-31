64a67e3e153f68e407ee312adfd70258f2d25115
hahyun8587 <hahyun8587@gmail.com>
2021-05-31 23:23:33 +0900

    solved

--- binary_search/BOJ12015.c
+++ binary_search/BOJ12015.c
@@ -2,15 +2,27 @@
 
 #define MAX_SIZE 1000000
 
+int min(int a, int b);
 int _BS(int *arr, int s, int e, int x);
 int BS(int *arr, int n, int x);
 int LIS(int *arr, int n);
 
 int main() {
+    int arr[MAX_SIZE];
+    int N;
 
+    scanf("%d", &N);
 
+    for (int i = 0; i < N; i++)
+        scanf("%d", &arr[i]);
+
+    printf("%d\n", LIS(arr, N));    
 
     return 0;
+}
+
+int min(int a, int b) {
+    return a < b ? a : b;
 }
 
 int _BS(int *arr, int s, int e, int x) {
@@ -22,8 +34,26 @@ int _BS(int *arr, int s, int e, int x) {
     if (x == arr[mid])
         return mid;
     else if (x < arr[mid]) 
-        return 
+        return min(_BS(arr, s, mid - 1, x), mid);
     else 
         return _BS(arr, mid + 1, e, x);    
 }
 
+int BS(int *arr, int n, int x) {
+    return _BS(arr, 0, n - 1, x);
+}
+
+int LIS(int *arr, int n) {
+    int ia[MAX_SIZE];
+    int size = 1;
+
+    ia[0] = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > ia[size - 1])
+            ia[size++] = arr[i];
+        else
+            ia[BS(ia, size, arr[i])] = arr[i];
+    }
+    return size;
+}
