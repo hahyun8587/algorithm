96110d031dcfa4d76076f375573d16370b84043c
Guest <hahyun8587@gmail.com>
2021-06-02 20:27:03 +0900

    solving git push
    asdf

--- heap/BOJ11279.c
+++ heap/BOJ11279.c
@@ -15,6 +15,7 @@ int ascn(int *arr, int i, int j);
 int descn(int *arr, int i, int j);
 int adescn(int *arr, int i, int j);
 heap *initHeap(int n);
+void swap(int *a, int *b);
 void swim(heap *h, int x);
 void sink(heap *h, int s);
 int delete(heap *h);
@@ -43,9 +44,29 @@ heap *initHeap(int n) {
     int (*type[NUM_TYPE])(int *, int, int) = {ascn, descn, adescn};
 
     h = (heap *) malloc(sizeof(heap));
-    h->ha = (int *) malloc(sizeof(int) * n);
-    h->n = n;
+    h->ha = (int *) malloc(sizeof(int) * (n + 1));
+    h->n = n + 1;
     h->type = type[ORDER];
 
     return h;
 }
+
+void swap(int *a, int *b) {
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void swim(heap *h, int x) {
+    h->ha[h->n++] = x;
+
+    for (int i = h->n - 1; i > 1 ; i /= 2) {
+        if (h->type(h->ha, i, i / 2))
+            swap(&h->ha[i], &h->ha[i / 2]);
+        else 
+            break;    
+    }
+}
+
