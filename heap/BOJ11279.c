fa1d6120fcdf2485a3943b75ed0d7c9b8459e006
Guest <hahyun8587@gmail.com>
2021-06-02 23:38:00 +0900

    solved

--- heap/BOJ11279.c
+++ heap/BOJ11279.c
@@ -4,6 +4,7 @@
 
 #define NUM_TYPE 3
 #define ORDER 0
+#define EMPTY_VAL 0
 
 typedef struct {
     int *ha;
@@ -15,15 +16,33 @@ int ascn(int *arr, int i, int j);
 int descn(int *arr, int i, int j);
 int adescn(int *arr, int i, int j);
 heap *initHeap(int n);
+int empty(heap *h);
 void swap(int *a, int *b);
 void swim(heap *h, int x);
 void sink(heap *h, int s);
 int delete(heap *h);
+void freeHeap(heap *h);
 
 int main() {
+    heap *h;
+    int N;
 
+    scanf("%d", &N);
 
+    h = initHeap(N);
 
+    for (int i = 1; i <= N; i++) {
+        int x;
+
+        scanf("%d", &x);
+        
+        if (x)
+            swim(h, x);
+        else
+            empty(h) ? printf("%d\n", EMPTY_VAL) : printf("%d\n", delete(h));  
+    }
+    freeHeap(h);
+    
     return 0;
 }
 
@@ -45,10 +64,14 @@ heap *initHeap(int n) {
 
     h = (heap *) malloc(sizeof(heap));
     h->ha = (int *) malloc(sizeof(int) * (n + 1));
-    h->n = n + 1;
+    h->n = 1;
     h->type = type[ORDER];
 
     return h;
+}
+
+int empty(heap *h) {
+    return h->n == 1 ? 1 : 0;
 }
 
 void swap(int *a, int *b) {
@@ -70,3 +93,36 @@ void swim(heap *h, int x) {
     }
 }
 
+void sink(heap *h, int s) {
+    if (s * 2 > h->n - 1)
+        return;
+
+    int curr = s;
+    int cmp;
+
+    do {
+        if (curr * 2 == h->n - 1)
+            cmp = curr * 2;
+        else
+            cmp = h->type(h->ha, curr * 2, curr * 2 + 1) ? curr * 2 : curr * 2 + 1;
+
+        if (h->type(h->ha, cmp, curr))
+            swap(&h->ha[curr], &h->ha[cmp]);
+        else 
+            break;
+
+        curr = cmp;
+    } while (curr * 2 <= h->n - 1); 
+}
+
+int delete(heap *h) {
+    swap(&h->ha[1], &h->ha[h->n-- - 1]);
+    sink(h, 1);
+
+    return h->ha[h->n];
+}
+
+void freeHeap(heap *h) {
+    free(h->ha);
+    free(h);
+}
