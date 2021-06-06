4fa8f758ba4be4eba08739760e35921fb6080a49
Guest <hahyun8587@gmail.com>
2021-06-06 23:10:52 +0900

    solved

--- DP/BOJ11054.c
+++ DP/BOJ11054.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
+#define MAX_SIZE 1000
+
 int forward(int a, int b);
 int backward(int a, int b);
 int max(int a, int b);
-int amax(int *arr, int n);
 void LIS(int *arr, int n, int *dp, int reverse);
 int LBS(int *arr, int n);
 
 int main() {
+    int arr[MAX_SIZE];
+    int N;
 
+    scanf("%d", &N);
 
+    for (int i = 0; i < N; i++)
+        scanf("%d", &arr[i]);
+
+    printf("%d\n", LBS(arr, N));
 
     return 0;
 }
@@ -26,28 +34,32 @@ int max(int a, int b) {
     return a > b ? a : b;
 }
 
-int amax(int *arr, int n) {
-    int val = 0;
-
-    for (int i = 0; i < n; i++) 
-        val = max(arr[i], val); 
-
-    return val;
-}
-
 void LIS(int *arr, int n, int *dp, int reverse) {
     int (*fp[])(int, int) = { forward, backward };
 
-    dp[(n - 1) * reverse + 1] += 1;
+    dp[0] = 0;
+    dp[(n - 1) * reverse + 1] = 1;
 
     for (int i = (n - 3) * reverse + 2; fp[reverse](i, n - (n - 1) * reverse); i += -2 * reverse + 1) {
         int val = 0;
 
-        for (int j = (n - 1) * reverse; fp[reverse](j, i - 2 + 2 * reverse); i += 2 * reverse - 1) {
-                    
+        for (int j = (n - 1) * reverse; fp[reverse](j, i - 2 + 2 * reverse); j += -2 * reverse + 1) {
+            if (arr[i - 1] > arr[j])
+                val = max(dp[j + 1], val);               
         }
+        dp[i] = val + 1;
     }        
+}
 
+int LBS(int *arr, int n) {
+    int dp1[MAX_SIZE + 1], dp2[MAX_SIZE + 1];
+    int ans = 0;
 
+    LIS(arr, n, dp1, 0);
+    LIS(arr, n, dp2, 1);
 
+    for (int i = 1; i <= n; i++)
+        ans = max(dp1[i] + dp2[i] - 1, ans);
+
+    return ans;
 }
