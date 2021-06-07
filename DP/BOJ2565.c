121ecf0a8f0377f566da5a500344a494076a2872
hahyun8587 <hahyun8587@gmail.com>
2021-06-07 19:23:00 +0900

    solved

---
+++ DP/BOJ2565.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int ascn(int **arr, int i, int j, int std);
+void bubbleSort(int **arr, int std);
+void LIS(int **arr, int n, int std);
+int minCord(int **arr, int n);
+
+int main() {
+
+
+
+    return 0;
+}
+
+int ascn(int **arr, int i, int j, int std) {
+    
+
+
+
+}
