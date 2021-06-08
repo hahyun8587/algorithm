37bcf5b46caab682b9fd3b3d2b53c06f64b05e6c
Guest <hahyun8587@gmail.com>
2021-06-08 20:25:41 +0900

    solving

--- DP/BOJ2565.c
+++ DP/BOJ2565.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <malloc.h>
 
-int ascn(int **arr, int i, int j, int std);
-void bubbleSort(int **arr, int std);
+#define MAX_SIZE 100
+
+int **mmalloc(int n, int m);
+void swap(int **a, int **b);
+void bubbleSort(int **arr, int n, int std);
 void LIS(int **arr, int n, int std);
 int minCord(int **arr, int n);
 
@@ -13,9 +16,30 @@ int main() {
     return 0;
 }
 
-int ascn(int **arr, int i, int j, int std) {
-    
+int **mmalloc(int n, int m) {
+    int **arr;
 
+    arr = (int **) malloc(sizeof(int *) * n);
 
+    for (int i = 0; i < n; i++)
+        arr[i] = (int *) malloc(sizeof(int) * m);
 
+    return arr;
+}
+
+void swap(int **a, int **b) {
+    int *temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int **arr, int n, int std) {
+    for (int i = 0; i <= n - 2; i++) {
+        for (int j = 0; j <= n - i - 2; j++) {
+            if (arr[j][std] > arr[j + 1][std])
+                swap(&arr[j], &arr[j + 1]);
+        }
+    }
 }
