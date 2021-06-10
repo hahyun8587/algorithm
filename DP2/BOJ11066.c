c39935bbbe427caaadf2e42d260711bcf9e53354
Guest <hahyun8587@gmail.com>
2021-06-10 23:51:25 +0900

    solving

--- DP2/BOJ11066.c
+++ DP2/BOJ11066.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define MAX_SIZE 500
+
 int min(int a, int b);
 int minCost(int *arr, int n);
 
@@ -10,3 +12,15 @@ int main() {
     return 0;
 }
 
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int minCost(int *arr, int n) {
+    int DP[MAX_SIZE + 1][MAX_SIZE + 1];
+
+    
+
+
+
+}
