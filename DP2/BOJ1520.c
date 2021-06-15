94ef33908a70a3cb4eb9507ff9ce4c4fd2e70ddf
Guest <hahyun8587@gmail.com>
2021-06-15 23:19:08 +0900

    solving

---
+++ DP2/BOJ1520.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MAX_SIZE 500
+
+int path(int (*arr)[MAX_SIZE], int n);
+
+int main() {
+
+
+
+    return 0;
+}
+
+int path(int (*arr)[MAX_SIZE], int n) {
+    
+
+
+
+
+}
