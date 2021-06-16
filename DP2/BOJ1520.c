64a3d2d345916b32cf516bc4fc73939fb299a327
Guest <hahyun8587@gmail.com>
2021-06-16 23:10:56 +0900

    solving

--- DP2/BOJ1520.c
+++ DP2/BOJ1520.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 #define MAX_SIZE 500
+#define NUM_WAY 4
 
-int path(int (*arr)[MAX_SIZE], int n);
+int _path(int (*arr)[MAX_SIZE], int n, int m, int (*dp)[MAX_SIZE], int x, int y);
+int path(int (*arr)[MAX_SIZE], int n, int m);
 
 int main() {
 
@@ -11,8 +13,17 @@ int main() {
     return 0;
 }
 
-int path(int (*arr)[MAX_SIZE], int n) {
-    
+int _path(int (*arr)[MAX_SIZE], int n, int m, int (*dp)[MAX_SIZE], int x, int y) {
+    if (dp[x][y] != -1)
+        return dp[x][y];
+
+    dp[x][y] = 0;
+
+    for (int i = 0; i < NUM_WAY; i++) {
+        if ()
+
+    }
+
 
 
 
