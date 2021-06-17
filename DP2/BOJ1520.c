a7149331eb7d7bba97abf79ee9d1cd3ecb14ea15
Guest <hahyun8587@gmail.com>
2021-06-17 23:00:02 +0900

    solved

--- DP2/BOJ1520.c
+++ DP2/BOJ1520.c
@@ -6,8 +6,16 @@ int _path(int (*arr)[MAX_SIZE], int n, int m, int (*dp)[MAX_SIZE], int x, int y)
 int path(int (*arr)[MAX_SIZE], int n, int m);
 
 int main() {
+    int arr[MAX_SIZE][MAX_SIZE];
+    int M, N;
 
+    scanf("%d %d", &M, &N);
 
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++)
+            scanf("%d", &arr[i][j]);
+    }
+    printf("%d\n", path(arr, M, N));
 
     return 0;
 }
@@ -19,7 +27,7 @@ int _path(int (*arr)[MAX_SIZE], int n, int m, int (*dp)[MAX_SIZE], int x, int y)
     dp[x][y] = 0;
 
     for (int i = 0; i <= 3; i++) {
-        int j = i ? i + x : x;
+        int j = i ? i + x - 2 : x;
         int k = i < 3 ? i + y - 1 : y; 
 
         if (j >= 0 && j < n && k >= 0 && k < m) {
@@ -31,8 +39,13 @@ int _path(int (*arr)[MAX_SIZE], int n, int m, int (*dp)[MAX_SIZE], int x, int y)
 }
 
 int path(int (*arr)[MAX_SIZE], int n, int m) {
-    
+    int dp[MAX_SIZE][MAX_SIZE];
 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++)
+            dp[i][j] = -1;
+    }
+    dp[n - 1][m - 1] = 1;
 
-
+    return _path(arr, n, m, dp, 0, 0);
 }
