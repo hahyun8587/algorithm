645a7771d0b503348d0893a6f554daec40aaf71b
Guest <hahyun8587@gmail.com>
2021-06-19 20:26:16 +0900

    solving

--- DP2/BOJ10942.c
+++ DP2/BOJ10942.c
@@ -5,9 +5,25 @@
 void palin(int *arr, int n, int (*dp)[MAX_SIZE + 1]);
 
 int main() {
+    int dp[MAX_SIZE + 1][MAX_SIZE + 1];
+    int arr[MAX_SIZE];
+    int N;
+    int M;
 
+    scanf("%d", &N);
 
+    for (int i = 0; i < N; i++)
+        scanf("%d", &arr[i]);
 
+    palin(arr, N, dp);
+    scanf("%d", &M);
+
+    for (int i = 1; i <= M; i++) {
+        int S, E;
+
+        scanf("%d %d", &S, &E);
+        printf("%d\n", dp[E - S + 1][S]);
+    }
     return 0;
 }
 
@@ -21,13 +37,7 @@ void palin(int *arr, int n, int (*dp)[MAX_SIZE + 1]) {
         dp[i][0] = 1;
 
     for (int i = 2; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (i + j - 1 > n)
-                dp[i][j] = 0;           
-            else { 
-                if (dp[i - 2][j + 1] && arr[j - 1] == arr[i + j - 2])
-                    dp[i][j] = 1;
-            }
-        }
+        for (int j = 1; j <= n; j++) 
+            dp[i][j] = i + j - 1 <= n && dp[i - 2][j + 1] && arr[j - 1] == arr[i + j - 2] ? 1 : 0;
     }
 }
