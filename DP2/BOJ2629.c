d2f5c8a85f8079770c4023cc04173a5b0bfabaf3
Guest <hahyun8587@gmail.com>
2021-06-23 20:20:26 +0900

    solved

--- DP2/BOJ2629.c
+++ DP2/BOJ2629.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 
 #define MAX_PNUM 30
-#define MAX_WEI 40000
+#define MAX_MWEI 40000
 #define MAX_MNUM 7
+#define MAX_PWEI 500
 
-void valid(int *arr, int n, int (*dp)[MAX_WEI + 1]);
+void valid(int *arr, int n, int (*dp)[MAX_MWEI + 1]);
 
 int main() {
-    int dp[MAX_PNUM + 1][MAX_WEI + 1];
+    int dp[MAX_PNUM + 1][MAX_MWEI + 1];
     int pw[MAX_PNUM], mw[MAX_MNUM];
     int N, M;
 
@@ -31,14 +32,18 @@ int main() {
     return 0;
 }
 
-void valid(int *arr, int n, int (*dp)[MAX_WEI + 1]) {
+void valid(int *arr, int n, int (*dp)[MAX_MWEI + 1]) {
     dp[0][0] = 1;
 
-    for (int i = 1; i <= MAX_WEI; i++) 
+    for (int i = 1; i <= MAX_MWEI; i++) 
         dp[0][i] = 0;
 
     for (int i = 1; i <= n; i++) {
-        for (int j = 0; j <= MAX_WEI; j++) 
-            dp[i][j] = dp[i - 1][j] || dp[i - 1][abs(j - arr[i - 1])] || dp[i - 1][j + arr[i - 1]] ? 1 : 0;
+        for (int j = 0; j <= MAX_MWEI; j++) {
+            if (j > MAX_PNUM * MAX_PWEI)
+                dp[i][j] = 0;
+            else
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][abs(j - arr[i - 1])] || dp[i - 1][j + arr[i - 1]] ? 1 : 0;
+        }
     }
 }
