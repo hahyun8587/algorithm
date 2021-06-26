3da3b92bbbf9c96e2ca6c6bb70ca9b90b8ff2369
Guest <hahyun8587@gmail.com>
2021-06-26 14:22:58 +0900

    solving

--- DP2/BOJ2293.c
+++ DP2/BOJ2293.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define MAX_SIZE 2
+#define MAX_TARGET 10000
+
 int numCase(int *arr, int n, int t);
 
 int main() {
@@ -10,8 +13,14 @@ int main() {
 }
 
 int numCase(int *arr, int n, int t) {
-    
+    int dp[MAX_SIZE][MAX_TARGET + 1];    
 
+    dp[0][0] = 1;
+
+    for (int i = 1; i <= t; i++)
+        dp[0][i] = 0;
+
+    
 
 
 }
