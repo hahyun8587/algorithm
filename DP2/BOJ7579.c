5a5c83757fc574eabd1f719ed9a7beb9b18aaa88
Guest <hahyun8587@gmail.com>
2021-06-27 20:26:12 +0900

    solved

--- DP2/BOJ7579.c
+++ DP2/BOJ7579.c
@@ -9,8 +9,18 @@ int min(int a, int b);
 int minCost(int *byte, int n, int *cost, int m);
 
 int main() {
+    int byte[MAX_ANUM], cost[MAX_ANUM];
+    int N, M;
 
+    scanf("%d %d", &N, &M);
 
+    for (int i = 0; i < N; i++)
+        scanf("%d", &byte[i]);
+
+    for (int i = 0; i < N; i++)
+        scanf("%d", &cost[i]);
+
+    printf("%d\n", minCost(byte, N, cost, M));    
 
     return 0;
 }
@@ -22,6 +32,14 @@ int min(int a, int b) {
 int minCost(int *byte, int n, int *cost, int m) {
     int dp[MAX_SIZE][MAX_MEM + 1];
 
-    
+    dp[0][0] = 0;
 
+    for (int i = 1; i <= m; i++)
+        dp[0][i] = INF;
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= m; j++)
+            dp[i % 2][j] = min(dp[(i + 1) % 2][j], cost[i - 1] + (byte[i - 1] < j ? dp[(i + 1) % 2][j - byte[i - 1]] : 0));
+    }
+    return dp[n % 2][m];
 }
