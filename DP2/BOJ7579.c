6846a3590c7c86a5597b4768d2d7d851b0a5f301
Guest <hahyun8587@gmail.com>
2021-06-27 14:54:31 +0900

    solving

--- DP2/BOJ7579.c
+++ DP2/BOJ7579.c
@@ -2,7 +2,8 @@
 
 #define MAX_ANUM 100 
 #define MAX_SIZE 2
-#define INF 10001
+#define MAX_MEM 10000000
+#define INF 10000
 
 int min(int a, int b);
 int minCost(int *byte, int n, int *cost, int m);
@@ -15,5 +16,12 @@ int main() {
 }
 
 int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int minCost(int *byte, int n, int *cost, int m) {
+    int dp[MAX_SIZE][MAX_MEM + 1];
+
     
+
 }
