dde598f46c23f5f757be189059616f8eb9770ccc
hahyun8587 <hahyun8587@gmail.com>
2021-07-04 20:30:49 +0900

    solving

--- BFS_and_DFS/BOJ2667.c
+++ BFS_and_DFS/BOJ2667.c
@@ -10,9 +10,16 @@ typedef struct {
     int tail;
 } queue;
 
+queue *initQueue(int n);
+int size(queue *q);
+int push(queue *q, int x);
+int empty(queue *q);
+int pop(queue *q);
+void freeQueue(queue *q);
 int BFS(int (*loc)[MAX_SIZE], int n, int *visited, int sx, int sy);
 void bubbleSort(int *arr, int n);
 void numBlock(int (*src)[MAX_SIZE], int n, int *dst, int *m);
+
 
 int main() {
 
@@ -22,9 +29,29 @@ int main() {
 }
 
 int BFS(int (*loc)[MAX_SIZE], int n , int *visited, int sx, int sy) {
+    queue *q;
     int count = 0;
 
-        
+    q = initQueue(n * n - 1);
 
+    if (loc[sx][sy]) {
+        push(q, n * sx + sy);
+
+        visited[n * sx + sy] = 1;
+        count++;
+    }
+
+    while (!empty(q)) {
+        int node;
+        int x, y;
+
+        node = pop(q);
+        x = node / n;
+        y = node % n; 
+
+        for (int i = 0; i < 4; i++) {
+            
+        }
+    }
 
 }
