fd28123b3d56d5c930621a6f167f7ea58a6df1c9
Guest <hahyun8587@gmail.com>
2021-07-04 23:50:34 +0900

    solving

--- BFS_and_DFS/BOJ2667.c
+++ BFS_and_DFS/BOJ2667.c
@@ -17,6 +17,7 @@ int empty(queue *q);
 int pop(queue *q);
 void freeQueue(queue *q);
 int BFS(int (*loc)[MAX_SIZE], int n, int *visited, int sx, int sy);
+void swap(int *a, int *b);
 void bubbleSort(int *arr, int n);
 void numBlock(int (*src)[MAX_SIZE], int n, int *dst, int *m);
 
@@ -50,8 +51,57 @@ int BFS(int (*loc)[MAX_SIZE], int n , int *visited, int sx, int sy) {
         y = node % n; 
 
         for (int i = 0; i < 4; i++) {
-            
+            int j = i > 0 ? i + x - 2 : x;
+            int k = i < 3 ? i + y - 1 : y;
+            int l = node + (2 * (i / 2) - 1) * ((n - 1) * (i % 2) + 1);
+
+            if (loc[j][k] && !visited[l]) {
+                push(q, l);
+
+                visited[l] = 1;
+                count++;
+            }
         }
     }
+    return count;
+}
+
+void swap(int *a, int *b) {
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int *arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < -i + n; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);         
+        }
+    }
+}
+
+void numBlock(int (*src)[MAX_SIZE], int n, int *dst, int *m) {
+    int visited[MAX_SIZE * MAX_SIZE];
+    int count = 0;
+
+    for (int i = 0; i < n * n; i++)
+        visited[i] = 0;        
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (src[i][j] && !visited[n * i + j]) {
+                int len;
+
+                if (len = BFS(src, n, visited, i, j) > 1)
+                    visited[count++] = len;
+            }
+        }
+    }
+    
 
 }
+
+
