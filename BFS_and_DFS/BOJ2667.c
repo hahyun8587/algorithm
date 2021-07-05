5257efec1d575d47ad96bf324545afc3490ba824
hahyun8587 <hahyun8587@gmail.com>
2021-07-05 23:28:20 +0900

    solved

--- BFS_and_DFS/BOJ2667.c
+++ BFS_and_DFS/BOJ2667.c
@@ -21,11 +21,23 @@ void swap(int *a, int *b);
 void bubbleSort(int *arr, int n);
 void numBlock(int (*src)[MAX_SIZE], int n, int *dst, int *m);
 
-
 int main() {
+    int loc[MAX_SIZE][MAX_SIZE];
+    int blocks[MAX_SIZE * MAX_SIZE / 2];
+    int N, M;
+
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) 
+            scanf("%1d", &loc[i][j]);
+    }
+    numBlock(loc, N, blocks, &M);
+    printf("%d\n", M);
+
+    for (int i = 0; i < M; i++)
+        printf("%d\n", blocks[i]);
     
-
-
     return 0;
 }
 
@@ -38,6 +50,36 @@ queue *initQueue(int n) {
     q->tail = q->head = 0;
 
     return q;
+}
+
+int size(queue *q) {
+    return q->head >= q->tail ? q->head - q->tail : q->n + q->head - q->tail;
+}
+
+int push(queue *q, int x) {
+    if (size(q) == q->n - 1)
+        return -1;
+
+    q->qa[q->head++] = x;
+    q->head %= q->n;        
+
+    return x;
+}
+
+int empty(queue *q) {
+    return q->head == q->tail ? 1 : 0;
+}
+
+int pop(queue *q) {
+    if (empty(q))
+        return -1;
+
+    int val;
+
+    val = q->qa[q->tail++];
+    q->tail %= q->n;    
+
+    return val;
 }
 
 int BFS(int (*loc)[MAX_SIZE], int n , int *visited, int sx, int sy) {
@@ -66,14 +108,18 @@ int BFS(int (*loc)[MAX_SIZE], int n , int *visited, int sx, int sy) {
             int k = i < 3 ? i + y - 1 : y;
             int l = node + (2 * (i / 2) - 1) * ((n - 1) * (i % 2) + 1);
 
-            if (loc[j][k] && !visited[l]) {
-                push(q, l);
+            if (j >= 0 && j < n && k >= 0 && k < n) {
+                if (loc[j][k] && !visited[l]) {
+                    push(q, l);
 
-                visited[l] = 1;
-                count++;
+                    visited[l] = 1;
+                    count++;
+                }
             }
         }
     }
+    freeQueue(q);
+
     return count;
 }
 
@@ -87,7 +133,7 @@ void swap(int *a, int *b) {
 
 void bubbleSort(int *arr, int n) {
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < -i + n; j++) {
+        for (int j = 0; j <= -i + n - 2; j++) {
             if (arr[j] > arr[j + 1])
                 swap(&arr[j], &arr[j + 1]);         
         }
@@ -103,12 +149,8 @@ void numBlock(int (*src)[MAX_SIZE], int n, int *dst, int *m) {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (src[i][j] && !visited[n * i + j]) {
-                int len;
-                
-                if (len = BFS(src, n, visited, i, j) > 1)
-                    dst[count++] = len;
-            }
+            if (src[i][j] && !visited[n * i + j]) 
+                dst[count++] = BFS(src, n, visited, i, j);
         }
     }
     bubbleSort(dst, count);
