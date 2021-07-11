53bec6687e24283e142d164c3d0035526baff679
Guest <hahyun8587@gmaIl.com>
2021-07-11 20:18:26 +0900

    solving

---
+++ BFS_and_DFS/BOJ7576.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define MAX_SIZE 1000
+
+int BFS(int (*arr)[MAX_SIZE], int n, int m, int *s);
+int minDate(int (*arr)[MAX_SIZE], int n, int m);
+
+int main() {
+
+
+
+    return 0;
+}
+
+int BFS(int (*Arr)[MAX_SIZE], int n, int m, int *s) {
+    
+
+
+
+}
+
