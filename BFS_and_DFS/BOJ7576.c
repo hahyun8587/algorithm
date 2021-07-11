6bc38857e9d843723a502acec95b01c06567db48
Guest <hahyun8587@gmail.com>
2021-07-11 23:01:10 +0900

    solving

--- BFS_and_DFS/BOJ7576.c
+++ BFS_and_DFS/BOJ7576.c
@@ -2,8 +2,17 @@
 #include <malloc.h>
 
 #define MAX_SIZE 1000
+#define NUM_AXIS 2
+#define NUM_COORD 1000000
 
-int BFS(int (*arr)[MAX_SIZE], int n, int m, int *s);
+typedef struct {
+    int *qa;
+    int n;
+    int head;
+    int tail;
+} queue;
+
+int BFS(int (*arr)[MAX_SIZE], int n, int m, int (*s)[NUM_AXIS], int p);
 int minDate(int (*arr)[MAX_SIZE], int n, int m);
 
 int main() {
@@ -13,8 +22,13 @@ int main() {
     return 0;
 }
 
-int BFS(int (*Arr)[MAX_SIZE], int n, int m, int *s) {
+int BFS(int (*arr)[MAX_SIZE], int n, int m, int (*s)[NUM_AXIS], int p) {
+    queue *q;    
+
+    q = initQueue(p == n * m ? n * m : n * m - 1);
+
     
+
 
 
 
